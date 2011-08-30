6d129b05168d5574b078bd1e820ab5801c5bd6e2
Braulio Bhavamitra <brauliobo@gmail.com>
2011-08-30 10:55:29 -0300

    add vi and gem configs

--- .bashmine.sh
+++ .bashmine.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+#RubyGEMS
+export PATH=$HOME/.gem/ruby/1.8/bin:$PATH
+
 alias "ack-grep"="ack-grep -a"
 alias "g"="git"
+alias vi="vim"                                                                                                                                                                                  
+alias vim='TERM="xterm-256color" vim'
+export EDITOR=vim                                                                                                                                                                               
