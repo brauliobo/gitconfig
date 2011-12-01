de8865a1d6ee8daf9d8a42f9c3774e08e75e1595
Braulio Bhavamitra <brauliobo@gmail.com>
2011-12-01 05:59:17 -0200

    Load noosfero rvm gemset by default

--- .bashmine.sh
+++ .bashmine.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#RubyGEMS
-export PATH=$HOME/.gem/ruby/1.8/bin:$PATH
-
 alias vi="vim"                                                                                                                                                                                  
 alias vim='TERM="xterm-256color" vim'
 export EDITOR=vim                                                                                                                                                                               
 alias "ack-grep"="ack-grep -a"
 alias "g"="git"
 complete -o default -o nospace -F _git g
+
+rvm 1.8.7@noosfero
