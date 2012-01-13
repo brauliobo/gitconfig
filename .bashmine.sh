e241bdaac47485c08c6a421a93982b5134f9a320
Braulio Bhavamitra <brauliobo@gmail.com>
2012-01-13 16:19:37 -0200

    Use ZSH!

--- .bashmine.sh
+++ .bashmine.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-alias vi="vim"                                                                                                                                                                                  
-alias vim='TERM="xterm-256color" vim'
-export EDITOR=vim                                                                                                                                                                               
-alias "ack-grep"="ack-grep -a"
-alias "g"="git"
-complete -o default -o nospace -F _git g
-
-rvm 1.8.7@noosfero
+zsh
--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-FILES=".bashmine.sh .gitconfig .vim .irbrc .screenrc"
-
+FILES=".bashmine.sh .zshmine.sh .gitconfig .vim .irbrc .screenrc"
 for f in $FILES; do
 	ln -s $PWD/$f $HOME/$f
 done
 ln -s $HOME/.vim/ian_vimrc $HOME/.vimrc
 
-echo '. $HOME/.bashmine.sh' >> ~/.bashrc
+if [[ ! -x $HOME/.rvm ]]; then
+	user$ bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
+fi
+if [[ ! -x $HOME/.oh-my-zsh ]]; then
+	wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+fi
+
+grep zshmine.sh $HOME/.zshrc > /dev/null
+if [[ 0 -ne $? ]]; then
+	echo '. $HOME/.zshmine.sh' >> ~/.zshrc
+fi
+grep bashmine.sh $HOME/.bashrc > /dev/null
+if [[ 0 -ne $? ]]; then
+	echo '. $HOME/.bashmine.sh' >> ~/.bashrc
+fi
