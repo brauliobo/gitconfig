38e69e74e5c770ef7e238b1dbe334c4d74567d75
Braulio Bhavamitra <brauliobo@gmail.com>
2012-01-20 23:27:16 -0200

    Restore bashmine.zh

--- .bashmine.sh
+++ .bashmine.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-zsh
+alias vi="vim"                                                                                                                                                                                  
+alias vim='TERM="xterm-256color" vim'
+export EDITOR=vim                                                                                                                                                                               
+alias "ack-grep"="ack-grep -a"
+alias "g"="git"
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+rvm 1.8.7@noosfero
