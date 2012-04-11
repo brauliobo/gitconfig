445f5fe4a757d919f5d851d1141b774c350f97cb
Braulio Bhavamitra <brauliobo@gmail.com>
2012-04-11 22:45:25 +0000

    Add .gemrc and .rvmrc configs and some others tweaks

--- .commonshmine.sh
+++ .commonshmine.sh
@@ -5,5 +5,9 @@ alias "ack-grep"="ack-grep -a"
 alias "g"="git"
 alias "git-new-workdir"="/usr/share/doc/git/contrib/workdir/git-new-workdir"
 
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-rvm 1.8.7@noosfero036
+# RVM
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" 
+PATH=$PATH:$HOME/.rvm/bin
+
+# default rvm gemset
+rvm 1.8.7@noosfero
--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-FILES=".commonshmine.sh .bashmine.sh .zshmine.sh .gitconfig .vim .irbrc .screenrc"
+# Link configurations files and scripts
+FILES=".commonshmine.sh .bashmine.sh .zshmine.sh .gitconfig .gemrc .rvmrc .vim .irbrc .screenrc"
 for f in $FILES; do
 	ln -s $PWD/$f $HOME/$f
 done
-ln -s $HOME/.vim/ian_vimrc $HOME/.vimrc
 
+# Finish VIM install
+ln -s $HOME/.vim/ian_vimrc $HOME/.vimrc
+# Install RVM
 if [[ ! -x $HOME/.rvm ]]; then
 	bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
 fi
+# Install Oh my ZSH
 if [[ ! -x $HOME/.oh-my-zsh ]]; then
 	wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 fi
 
+# Add autoload configs code
 grep zshmine.sh $HOME/.zshrc > /dev/null
 if [[ 0 -ne $? ]]; then
 	echo '. $HOME/.zshmine.sh' >> ~/.zshrc
