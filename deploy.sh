8569b1dbd879bc92f5ef6e86d9fb3003c1f9a73d
Braulio Bhavamitra <brauliobo@gmail.com>
2012-05-25 18:42:11 +0000

    Add git helpers from https://github.com/garybernhardt/dotfiles/blob/master/.githelpers

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 # Link configurations files and scripts
-FILES=".commonshmine.sh .bashmine.sh .zshmine.sh .gitconfig .gemrc .rvmrc .vim .irbrc .screenrc"
+FILES=".commonshmine.sh .bashmine.sh .zshmine.sh .gitconfig .githelpers .gemrc .rvmrc .vim .vimrc .irbrc .screenrc"
 for f in $FILES; do
 	ln -s $PWD/$f $HOME/$f
 done
 
-# Finish VIM install
-ln -s $HOME/.vim/ian_vimrc $HOME/.vimrc
 # Install RVM
 if [[ ! -x $HOME/.rvm ]]; then
 	bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
