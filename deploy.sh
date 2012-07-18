e4de34eeb4afcd72eba8c40b936054d0d7ae8f67
Braulio Bhavamitra <brauliobo@gmail.com>
2012-07-18 11:44:20 -0300

    Add -o|--overwrite option to overwrite config files

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
 
-function run {
+run() {
 	set -x; $@ ; { set +x; } 2>/dev/null
 }
 
-# Link configurations files not overwriting existing regular files
+# from http://stackoverflow.com/questions/9271381/how-can-i-parse-long-form-arguments-in-shell
+_setArgs() {
+    while [ "$1" != "" ]; do
+        case $1 in
+            "-o" | "--overwrite")
+                opt_overwrite=$1
+                ;;
+        esac
+        shift
+    done
+}
+_setArgs $*
+
+### Link configurations files not overwriting existing regular files
 for f in `ls -A configs`; do
-    [[ -L $HOME/$f ]] && rm $HOME/$f
+    [[ -L $HOME/$f || $opt_overwrite ]] && rm $HOME/$f
 	run ln -s $PWD/configs/$f $HOME/$f
 done
 
-# Update submodules
+### Update submodules
 run git submodule update --init
 
-# Install RVM
+### Install RVM
 if [[ ! -x $HOME/.rvm ]]; then
 	run bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
 fi
-# Install Oh my ZSH
+### Install Oh my ZSH
 if [[ ! -x $HOME/.oh-my-zsh ]]; then
 	run wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 fi
 
-# Add autoload configs code
-
+### Add autoload configs code
 if ! grep zshmine.sh $HOME/.zshrc > /dev/null; then
     run echo '. $HOME/.zshmine.sh' >> ~/.zshrc
 fi
-
 if ! grep bashmine.sh $HOME/.bashrc > /dev/null; then
 	run echo '. $HOME/.bashmine.sh' >> ~/.bashrc
 	run echo '. $HOME/.bashmine.sh' >> ~/.bash_profile
