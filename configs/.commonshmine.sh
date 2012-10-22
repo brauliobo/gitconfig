6d844750a466f5dc9645f09648353bbb1f0fda30
Braulio Bhavamitra <brauliobo@gmail.com>
2012-10-22 22:32:22 +0200

    Add default configuration file

--- configs/.commonshmine.sh
+++ configs/.commonshmine.sh
@@ -1,10 +1,21 @@
+# add scripts dir to $PATH
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+GITROOT="$( cd -P "$( dirname "$SOURCE" )" && git root )"
+DIR="$GITROOT/scripts"
+export PATH=$PATH:$DIR
+
+# Config
+source $GITROOT/default/config
+
 # VIM
 alias vi='TERM="xterm-256color" vim'
 alias vim='TERM="xterm-256color" vim'
 export EDITOR=vim
 
+# Grep
 alias "ack-grep"="ack-grep -a --sort-files"
 
+# Git
 alias "g"="git"
 alias "git-new-workdir"="/usr/share/doc/git/contrib/workdir/git-new-workdir"
 #export GIT_PROXY_COMMAND="proxy"
@@ -13,9 +24,4 @@ alias "git-new-workdir"="/usr/share/doc/git/contrib/workdir/git-new-workdir"
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 PATH=$PATH:$HOME/.rvm/bin
 # default gemset
-rvm ree@noosferoorig
-
-# add scripts dir to $PATH
-while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && git root )/scripts"
-export PATH=$PATH:$DIR
+rvm $DEFAULT_GEMSET
