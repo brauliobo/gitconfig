8bc6eed9549074c3e36aa589c7e283cd589878ee
Braulio Bhavamitra <brauliobo@gmail.com>
2012-11-13 11:52:56 +0000

    Add and install hooks

--- deploy.sh
+++ deploy.sh
@@ -17,43 +17,46 @@ _setArgs() {
 }
 _setArgs $*
 
-### Link configurations files not overwriting existing regular files
+echo == Link configurations files not overwriting existing regular files
 for f in `ls -A configs`; do
     [[ -L $HOME/$f || $opt_overwrite ]] && rm $HOME/$f
-	run ln -s $PWD/configs/$f $HOME/$f
+	ln -s $PWD/configs/$f $HOME/$f
 done
 
 while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
 GITROOT="$( cd -P "$( dirname "$SOURCE" )" && git root )"
-### Config
+echo == Source $GITROOT/default/config
 source $GITROOT/default/config
 
-### Update submodules
-run git submodule update --init
-run cd configs/.vim && git submodule update --init
+echo == Update submodules
+git submodule update --init
+cd configs/.vim && git submodule update --init
 
-### Install RVM
+echo == Install RVM
 if [[ ! -x $HOME/.rvm ]]; then
-	run curl -L https://get.rvm.io | bash -s stable
+	curl -L https://get.rvm.io | bash -s stable
 fi
-### Install Oh my ZSH
+echo == Install Oh my ZSH
 if [[ ! -x $HOME/.oh-my-zsh ]]; then
-	run wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+	wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 fi
 
-### Add autoload configs code
+echo == Add autoload configs code
 if ! grep zshmine.sh $HOME/.zshrc > /dev/null; then
-    run echo '. $HOME/.zshmine.sh' >> ~/.zshrc
+    echo '. $HOME/.zshmine.sh' >> ~/.zshrc
 fi
 if ! grep bashmine.sh $HOME/.bashrc > /dev/null; then
-	run echo '. $HOME/.bashmine.sh' >> ~/.bashrc
-	run echo '. $HOME/.bashmine.sh' >> ~/.bash_profile
+	echo '. $HOME/.bashmine.sh' >> ~/.bashrc
+	echo '. $HOME/.bashmine.sh' >> ~/.bash_profile
 fi
 
-### grab gems credentials
+echo == Grab gems credentials
 if [[ -n "$RUBYGEMS_USER" && ! -f ~/.gem/credentials ]]; then
     mkdir -p ~/.gem
-    run curl -u $RUBYGEMS_USER https://rubygems.org/api/v1/api_key.yaml > ~/.gem/credentials
+    curl -u $RUBYGEMS_USER https://rubygems.org/api/v1/api_key.yaml > ~/.gem/credentials
 fi
 
-
+echo == Install hooks
+for h in `ls $GITROOT/hooks`; do
+  ln -sf $GITROOT/hooks/$h $GITROOT/.git/hooks
+done
