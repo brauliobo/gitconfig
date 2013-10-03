bc3a28656e7eb5c01b5cd93fb29169ab56f8bbda
Braulio Bhavamitra <braulio@eita.org.br>
2013-10-03 10:06:47 -0300

    Support ack-grep 2.0 and 1.9

--- configs/.commonshmine.sh
+++ configs/.commonshmine.sh
@@ -1,26 +1,27 @@
 while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
 GITROOT="$( cd -P "$( dirname "$SOURCE" )" && git root )"
+SCRIPTS="$GITROOT/scripts"
+CUSTOM_PATHS=$SCRIPTS
 
 # Config
 source $GITROOT/default/config
 
-# add scripts dir to $PATH
-DIR="$GITROOT/scripts"
-export PATH=$PATH:$DIR
+# clear custom paths to avoid duplication
+PATH=`echo $PATH | sed "s|$CUSTOM_PATHS:||g"`
+
+# RVM
+source "$HOME/.rvm/scripts/rvm"
+rvm `cat $HOME/.ruby-version`
+
+# add custom paths
+export PATH=$CUSTOM_PATHS:$PATH
 
 # VIM
 alias vi='TERM="xterm-256color" vim'
 alias vim='TERM="xterm-256color" vim'
 export EDITOR=vim
 
-# Grep
-alias "ack-grep"="ack-grep -a --follow --sort-files"
-
 # Git
 alias "g"="git"
 alias "git-new-workdir"="/usr/share/doc/git/contrib/workdir/git-new-workdir"
 #export GIT_PROXY_COMMAND="proxy"
-
-# RVM
-source "$HOME/.rvm/scripts/rvm"
-rvm `cat $HOME/.ruby-version`
