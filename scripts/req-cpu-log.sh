bf8142c9fc8cbf4723161d79bf732fed916b2be6
Braulio Bhavamitra <braulio@eita.org.br>
2014-05-20 00:55:35 -0300

    Use full path

--- scripts/req-cpu-log.sh
+++ scripts/req-cpu-log.sh
@@ -2,6 +2,6 @@
 
 echo ---
 sar -u 1 1 | tail -n 3 | head -n 2
-reqs-per-sec.sh -f 1 -l /var/log/apache2/noosfero.access.log
+/root/gitconfig/scripts/reqs-per-sec.sh -f 1 -l /var/log/apache2/noosfero.access.log
 echo
 echo ---
