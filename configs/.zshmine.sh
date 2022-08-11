4c95ab5d6f9116455bf5279bb5e2e7a5d4cc26eb
Braulio Bhavamitra <brauliobo@gmail.com>
2022-08-11 17:56:29 -0300

    zshmine: configure PROMPT

--- configs/.zshmine.sh
+++ configs/.zshmine.sh
@@ -3,3 +3,5 @@
 SOURCE="$0"
 . ~/.commonshmine.sh
 
+PROMPT='%(!.%{%F{yellow}%}.)$USER @ %{$fg[white]%}%M %{$fg_bold[red]%}➜ %{$fg_bold[green]%}%p %{$fg[cyan]%}%c %{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%} % %{$reset_color%}'
+
