2b08754e174040e0dab1eca1c156cea302129028
Braulio Bhavamitra <brauliobo@gmail.com>
2024-10-31 09:07:01 -0300

    gui-login: comment swayidle

--- scripts/gui-login.sh
+++ scripts/gui-login.sh
@@ -7,11 +7,12 @@ if [[ $(lsmod | grep vfio) ]]; then
   sudo modprobe nvidia
   sudo nvidia-smi -pm 1
   sudo nvidia-smi -pl 100
+  #sudo nvidia-settings -a "[gpu:0]/GPUFanControlState=1"
+  #sudo nvidia-settings -a "[fan:0]/GPUTargetFanSpeed=90"
 fi
 
 if [ "$(loginctl show-session "$XDG_SESSION_ID" -p Type --value)" = "wayland" ]; then
-  systemctl --user enable --now swayidle
+  #systemctl --user enable --now swayidle
 else
-  systemctl --user disable swayidle
+  #systemctl --user disable swayidle
 fi
-
